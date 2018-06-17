85ed26517081c3188829aa934055d613d7c2ea37
Lam Chau <lamchau@gmail.com>
2018-06-16 23:12:17 -0700

    create headers utility object, remove headers from conversion

--- node/funding_raised.js
+++ node/funding_raised.js
@@ -2,67 +2,71 @@ const parseCsvSync = require('csv-parse/lib/sync');
 const fs = require('fs');
 const path = require('path');
 
-
-function parseFile(filename) {
-  const filepath = fs.readFileSync(path.join(__dirname, '..', filename)).toString()
-  return parseCsvSync(filepath);
-}
-
 class FundingRaised {
-  static getRowAsObject(headers, row) {
-    return headers.reduce((accumulator, headerName, index) => {
-      accumulator[headerName] = row[index];
-      return accumulator;
+  static _parseFile(filename) {
+    const filepath = fs.readFileSync(path.join(__dirname, '..', filename)).toString()
+    const rows = parseCsvSync(filepath);
+    const headers = rows[0];
+
+    FundingRaised.HEADERS = {
+      toName(index) {
+        return headers[index];
+      }
+    };
+    return rows.slice(1);
+  }
+
+  static _getRowAsObject(row) {
+    return row.reduce((object, value, index) => {
+      const headerName = FundingRaised.HEADERS.toName(index);
+      object[headerName] = value;
+      return object;
     }, {});
   }
 
   static where(options = {}) {
-    const rows = parseFile('startup_funding.csv');
-    const headers = rows[0];
-    let csv_data = rows.slice(1);
+    let rows = FundingRaised._parseFile('startup_funding.csv');
 
     if (options.company_name) {
-      csv_data = csv_data.filter(row => options.company_name == row[1]);
+      rows = rows.filter(row => options.company_name == row[1]);
     }
 
     if (options.city) {
-      csv_data = csv_data.filter(row => options.city == row[4]);
+      rows = rows.filter(row => options.city == row[4]);
     }
 
     if (options.state) {
-      csv_data = csv_data.filter(row => options.state == row[5]);
+      rows = rows.filter(row => options.state == row[5]);
     }
 
     if (options.round) {
-      csv_data = csv_data.filter(row => options.round == row[9]);
+      rows = rows.filter(row => options.round == row[9]);
     }
 
-    return csv_data.map(row => FundingRaised.getRowAsObject(headers, row));
+    return rows.map(row => FundingRaised._getRowAsObject(row));
   }
 
   static findBy(options = {}) {
-    const rows = parseFile('startup_funding.csv');
-    const headers = rows[0];
-    let csv_data = rows.slice(1);
+    let rows = FundingRaised._parseFile('startup_funding.csv');
 
     if (options.company_name) {
-      csv_data = csv_data.filter(row => options.company_name == row[1]);
-      return FundingRaised.getRowAsObject(headers, csv_data[0]);
+      rows = rows.filter(row => options.company_name == row[1]);
+      return FundingRaised._getRowAsObject(rows[0]);
     }
 
     if (options.city) {
-      csv_data = csv_data.filter(row => options.city == row[4]);
-     return FundingRaised.getRowAsObject(headers, csv_data[0]);
+      rows = rows.filter(row => options.city == row[4]);
+     return FundingRaised._getRowAsObject(rows[0]);
     }
 
     if (options.state) {
-      csv_data = csv_data.filter(row => options.state == row[5]);
-     return FundingRaised.getRowAsObject(headers, csv_data[0]);
+      rows = rows.filter(row => options.state == row[5]);
+     return FundingRaised._getRowAsObject(rows[0]);
     }
 
     if (options.round) {
-      csv_data = csv_data.filter(row => options.round == row[9]);
-     return FundingRaised.getRowAsObject(headers, csv_data[0]);
+      rows = rows.filter(row => options.round == row[9]);
+     return FundingRaised._getRowAsObject(rows[0]);
     }
   }
 }
